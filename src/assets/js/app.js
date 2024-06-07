4d15a0f929eff0bc5e14ebf3da9740bffad7c589
Jona <jonathan.diaz@driv.in>
2024-06-06 20:52:34 -0400

    update styles

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,24 +1,19 @@
+const incrementButton = document.querySelector('.increment-btn');
+const decrementButton = document.querySelector('.decrement-btn');
+const unitPrice = 400000;
 
-let priceContainer = document.querySelector(".initial-price");
-let price = 400000
+let priceContainer = document.querySelector('.initial-price');
+let totalPrice = document.querySelector('.total-price');
+let quantityContainer = document.querySelector('.quantity');
+let quantity = 0;
 
+const updatePrice = (action) => {
+  quantity = action == 'in' 
+    ? ++quantity 
+    : --quantity;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Init
-priceContainer.innerHTML = precio
+  if (quantity < 0) quantity = 0;
+  const total = quantity * unitPrice;
+  quantityContainer.innerHTML = quantity;
+  totalPrice.innerHTML = total;
+};
